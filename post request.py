2e83a22004d3af25ab946edc985b458955afb6a0
fatimaom <94124375+fatimaom@users.noreply.github.com>
2021-11-11 16:48:59 +0200

    Add files via upload

---
+++ post request.py
@@ -0,0 +1,24 @@
+# importing the requests library
+import requests
+  
+# defining the api-endpoint 
+API_ENDPOINT = “http://ec2-34-247-37-135.eu-west-1.compute.amazonaws.com:5000/handle”
+  
+
+  
+# data to be sent to api
+
+ data = { "type":"Single Battery",
+"org_id":"rmFT75nxhWsHSkTpo"
+,"bem_id":"PyTBT56vqaPt22mZ2",
+"email":"fatima.om11@gmail.com ",
+"Start":1635778688185,
+"End":1636383488185}
+} 
+  
+# sending post request and saving response as response object
+r = requests.post(url = API_ENDPOINT, data = data)
+  
+# extracting response text 
+pastebin_url = r.text
+print("The pastebin URL is:%s"%pastebin_url)
